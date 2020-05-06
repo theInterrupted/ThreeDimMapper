3c1ff8c64138a0ce8e0eb7ef072e9054c5b2c03b
Chuck M <mentalventure@gmail.com>
2020-05-06 09:04:15 -0400

    Update OBJLoader.js

--- js/OBJLoader.js
+++ js/OBJLoader.js
@@ -711,7 +711,7 @@ THREE.OBJLoader.prototype = {
 
 			var mesh;
 
-			/*if ( createdMaterials.length > 1 ) {
+			if ( createdMaterials.length > 1 ) {
 
 				for ( var mi = 0, miLen = materials.length; mi < miLen ; mi++ ) {
 
@@ -724,9 +724,9 @@ THREE.OBJLoader.prototype = {
 				mesh = ( ! isLine ? new THREE.Mesh( buffergeometry, multiMaterial ) : new THREE.LineSegments( buffergeometry, multiMaterial ) );
 
 			} else {
-			*/
+			
 				mesh = ( ! isLine ? new THREE.Mesh( buffergeometry, createdMaterials[ 0 ] ) : new THREE.LineSegments( buffergeometry, createdMaterials[ 0 ] ) );
-			//}
+			}
 
 			mesh.name = object.name;
 
