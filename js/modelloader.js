df3ffe9832be32f8935be6a9a6ec2c21e3c3a517
Chuck M <mentalventure@gmail.com>
2020-05-14 11:31:19 -0400

    Update modelloader.js

--- js/modelloader.js
+++ js/modelloader.js
@@ -163,8 +163,8 @@ function createThreeLayer(){
 
 				gltf.scene.children.forEach(function(mesh){
 					if (mesh.type === "Mesh"){
-						//mesh.scale.set(.01,.01,.01);
-						mesh.scale.set(.1,.1,.1);
+						mesh.scale.set(.01,.01,.01);
+						//mesh.scale.set(.1,.1,.1);
 						mesh.rotation.set(0, 0, (Math.PI/2)-(d.az * Math.PI / 180));
 						var model=threeLayer.toModel(mesh,{
 							coordinate:new maptalks.Coordinate(d.lon,d.lat),
