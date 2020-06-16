7129b6cc9f87779116ec72f7a4dcec5c8a2086d6
Chuck M <mentalventure@gmail.com>
2020-06-16 09:42:27 -0400

    ahhhhhhhhhhh

--- js/modelloader.js
+++ js/modelloader.js
@@ -6,7 +6,7 @@
 //	zoom functionality
 // TODO:
 
-const version = 3.20;
+const version = 3.21;
 d3.select("title").text("Data-Driven 3D Maps " + version);
 
 const poi = [-84.22550713006798,39.9001169544084];//Dayton Intl Airport
